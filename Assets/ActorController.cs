9ae8764385d97b33eca327cdacdf453b62131e62
zzhzll <m_13352210621_2@163.com>
2022-03-15 00:11:53 +0800

    zzh_first

---
+++ Assets/ActorController.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ActorController : MonoBehaviour
+{
+    public GameObject model;
+    public PlayerInput playerInput;
+
+    [SerializeField]
+    private Animator anim;
+
+    void Awake()
+    {
+        playerInput = GetComponent<PlayerInput>();
+        anim = model.GetComponent<Animator>();
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // print(playerInput.Dup);
+        anim.SetFloat("forward", playerInput.Dmag);
+        model.transform.forward = playerInput.Dvec;
+    }
+}
---
+++ Assets/PlayerInput.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerInput : MonoBehaviour
+{
+    public string keyUp;
+    public string keyDown;
+    public string keyLeft;
+    public string keyRight;
+
+    public float Dup;
+    public float Dright;
+    public float Dmag;
+    public Vector3 Dvec;
+
+    public bool inputEnabled = true;
+
+    private float targetDup;
+    private float targetDright;
+    private float velocityDup;
+    private float velocityDright;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        targetDup = (Input.GetKey(keyUp) ? 1.0f : 0) - (Input.GetKey(keyDown) ? 1.0f : 0);
+        targetDright = (Input.GetKey(keyRight) ? 1.0f : 0) - (Input.GetKey(keyLeft) ? 1.0f : 0);
+
+        if(inputEnabled == false)
+        {
+            targetDup = 0;
+            targetDright = 0;
+        }
+        
+        Dup = Mathf.SmoothDamp(Dup, targetDup, ref velocityDup, 0.1f);
+        Dright = Mathf.SmoothDamp(Dright, targetDright, ref velocityDright, 0.1f);
+        Dmag = Mathf.Sqrt((Dup * Dup) + (Dright * Dright));
+        Dvec = Dright * transform.right + Dup * transform.forward;
+    }
+}
