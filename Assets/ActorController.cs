d71322c3fcca3e5a57a1923aa438f425812dcbfc
zzhzll <m_13352210621_2@163.com>
2022-03-25 00:08:51 +0800

    zzh

--- Assets/ActorController.cs
+++ Assets/ActorController.cs
@@ -7,13 +7,22 @@ public class ActorController : MonoBehaviour
     public GameObject model;
     public PlayerInput playerInput;
 
+    public float walkSpeed = 1.4f;
+    public float runMultiplier = 3.0f;
+
+
     [SerializeField]
     private Animator anim;
+
+    private Rigidbody rigid;
+
+    private Vector3 movingVec;
 
     void Awake()
     {
         playerInput = GetComponent<PlayerInput>();
         anim = model.GetComponent<Animator>();
+        rigid = GetComponent<Rigidbody>();
     }
 
     // Start is called before the first frame update
@@ -23,10 +32,30 @@ public class ActorController : MonoBehaviour
     }
 
     // Update is called once per frame
+    // 每秒跑60次
     void Update()
     {
+        // 两帧间隔 Time.deltaTime
         // print(playerInput.Dup);
-        anim.SetFloat("forward", playerInput.Dmag);
-        model.transform.forward = playerInput.Dvec;
+        var real_runMultiplier = ((playerInput.run) ? 2.0f : 1.0f);
+        anim.SetFloat("forward", playerInput.Dmag * real_runMultiplier);
+        if (playerInput.Dmag > 0.1f)
+        {
+            // TODO
+            model.transform.forward = playerInput.Dvec;
+        }
+        // 玩家的移动量 * 当前的方向
+        movingVec = playerInput.Dmag * model.transform.forward * walkSpeed * real_runMultiplier;
+    }
+
+    // 物理引擎每秒模拟50遍 
+    void FixedUpdate()
+    {
+        // 物理引擎两帧间隔Time.fixedDeltaTime
+        // 改位置：速度*时间
+        rigid.position += movingVec * Time.fixedDeltaTime;
+        // 直接修改速度
+        // TODO
+        // rigid.velocity = new Vector3(movingVec.x, rigid.velocity.y, movingVec.z);
     }
 }
--- Assets/PlayerInput.cs
+++ Assets/PlayerInput.cs
@@ -9,11 +9,23 @@ public class PlayerInput : MonoBehaviour
     public string keyLeft;
     public string keyRight;
 
+    public string keyRunA;
+    public string keyB;
+    public string keyC;
+    public string keyD;
+
+    [Header("===== Output signals =====")]
     public float Dup;
     public float Dright;
     public float Dmag;
     public Vector3 Dvec;
 
+    // 1.pressing signal
+    public bool run;
+    // 2.trigger once signal
+    // 3.double trigger
+
+    [Header("===== Others =====")]
     public bool inputEnabled = true;
 
     private float targetDup;
@@ -24,7 +36,7 @@ public class PlayerInput : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-
+        keyRunA = "j";
     }
 
     // Update is called once per frame
@@ -33,15 +45,18 @@ public class PlayerInput : MonoBehaviour
         targetDup = (Input.GetKey(keyUp) ? 1.0f : 0) - (Input.GetKey(keyDown) ? 1.0f : 0);
         targetDright = (Input.GetKey(keyRight) ? 1.0f : 0) - (Input.GetKey(keyLeft) ? 1.0f : 0);
 
-        if(inputEnabled == false)
+        if (inputEnabled == false)
         {
             targetDup = 0;
             targetDright = 0;
         }
-        
+
+        // TODO
         Dup = Mathf.SmoothDamp(Dup, targetDup, ref velocityDup, 0.1f);
         Dright = Mathf.SmoothDamp(Dright, targetDright, ref velocityDright, 0.1f);
         Dmag = Mathf.Sqrt((Dup * Dup) + (Dright * Dright));
         Dvec = Dright * transform.right + Dup * transform.forward;
+
+        run = Input.GetKey(keyRunA);
     }
 }
